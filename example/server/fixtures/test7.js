c19ab13fd45d5c97b0b58c6e701d926e156290d5
Cheng Ly <chengsieuly@gmail.com>
2018-06-21 14:38:57 -0700

    Fix mapping issue of fixtures

---
+++ example/server/fixtures/test7.js
@@ -0,0 +1,11 @@
+module.exports = {
+    default: true,
+    description: "SEVENTH_CONTRACT",
+
+    url: '/api/test',
+
+    method: 'GET',
+    statusCode: 400,
+
+    data: 'error'
+};
--- src/server/helpers/setActiveResponses.js
+++ src/server/helpers/setActiveResponses.js
@@ -1,15 +1,16 @@
 module.exports = function setActiveResponses(fixtures) {
     return fixtures.reduce((map, fixture) => {
         const { method, url } = fixture;
-        
+
         // Only overwrite current existing if the fixture is the default.
         // Otherwise, leave as is.
         if (map[method] && map[method][url]) {
             if (fixture.default) {
-                return map[method] = {
+                map[method] = {
                     ...map[method],
                     [url]: fixture,
-                };
+                }
+                return map;
             }
 
             return map;
