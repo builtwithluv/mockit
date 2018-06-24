0e3092c16c4cd20bb21c83dc1cc65c2dbb5f6119
Cheng Ly <chengsieuly@gmail.com>
2018-06-23 20:49:44 -0700

    Create fixtures from har file

---
+++ src/.bin/unharify.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const createFixture = require('../helpers/createFixture');
+const config = require('../helpers/getConfig')();
+
+const harFilePath = path.resolve(config.harFilePath);
+const harContent = JSON.parse(fs.readFileSync(harFilePath, 'utf8'));
+const entries = harContent.log.entries;
+
+const supportedEntries = entries.filter(entry => {
+    const mimeType = entry.response.content.mimeType;
+    return mimeType === 'application/json';
+});
+
+supportedEntries.forEach(({ request, response }) => {
+    const { method, url } = request;
+    const { content, status } = response;
+    const { text } = content;
+
+    createFixture({
+        method,
+        status,
+        url,
+        data: JSON.parse(text),
+        description: 'TODO: Add description',
+    });
+});
---
+++ src/helpers/createFixture.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+const path = require('path');
+const uuidv4 = require('uuid/v4');
+const _url = require('url');
+const beautify = require('js-beautify');
+const { fixturesPath } = require('./getConfig')();
+
+module.exports = function createFixture({
+    data,
+    description,
+    filename,
+    url,
+    id = uuidv4(),
+    method = 'GET',
+    status = 200,
+}) {
+    const fileName = `${method}-${status}-${filename || id}.js`;
+
+    const fixture = {
+        id,
+        method,
+        description,
+        data,
+        url: _url.parse(url).pathname,
+        statusCode: status,
+    };
+
+    const beautifyOptions = {
+        indent_size: 4,
+        end_with_newline: true,
+    };
+
+    fs.writeFile(
+        path.resolve(path.join(fixturesPath, fileName)),
+        beautify(`module.exports = ${JSON.stringify(fixture)}`, beautifyOptions),
+        (err) => {
+            if (err) {
+                throw err;
+            }
+            console.log(`Saved file: ${fileName}`);
+        }
+    );
+}
