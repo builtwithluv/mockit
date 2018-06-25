ab19f12efce21393460ee2536da5608de5b20e24
Cheng Ly <chengsieuly@gmail.com>
2018-06-24 18:37:05 -0700

    Fix issues with command line

--- src/helpers/getConfig.js
+++ src/helpers/getConfig.js
@@ -5,10 +5,18 @@ module.exports = function getConfig() {
     const testyConfigurationFilePath = path.resolve('.testy');
     const defaultConfiguration = {
         fixturesPath: 'server/fixtures',
+        harFilePath: 'har.har',
     };
 
     if (fs.existsSync(testyConfigurationFilePath)) {
-        return JSON.parse(fs.readFileSync(testyConfigurationFilePath));
+        const content = JSON.parse(fs.readFileSync(testyConfigurationFilePath));
+        if (!content.hasOwnProperty('fixturesPath')) {
+            content.fixturesPath = defaultConfiguration.fixturesPath;
+        }
+        if (!content.hasOwnProperty('harFilePath')) {
+            content.harFilePath = defaultConfiguration.harFilePath;
+        }
+        return content;
     }
 
     return defaultConfiguration;
