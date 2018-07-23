56e873c60e099b8bacf5e5284eb941ea56a8e02d
Cheng Ly <chengsieuly@gmail.com>
2018-07-22 18:22:14 -0700

    Remove unharifying command

--- src/commands/unharify.js
+++ src/commands/unharify.js
@@ -1,18 +1,7 @@
-const fs = require('fs');
-const path = require('path');
 const createFixture = require('../helpers/createFixture');
-const config = require('../helpers/getConfig')();
 
 module.exports = function unharify(har) {
-    let entries;
-
-    if (!har) {
-        const harFilePath = path.resolve(config.harFilePath);
-        const harContent = JSON.parse(fs.readFileSync(harFilePath, 'utf8'));
-        entries = harContent.log.entries;
-    } else {
-        entries = har.log.entries;
-    }
+    const entries = har.log.entries;
 
     const supportedEntries = entries.filter(entry => {
         const mimeType = entry.response.content.mimeType;
