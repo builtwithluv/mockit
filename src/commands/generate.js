683b9fe6bef6f9dce63ba58f4f523c66a2fe6e28
Cheng Ly <chengsieuly@gmail.com>
2018-07-22 18:58:50 -0700

    Generate as a command

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -1,31 +1,21 @@
 const fetch = require('node-fetch');
+const createFixture = require('../helpers/createFixture');
 
-module.exports = function generate() {
-    const createFixture = require('../helpers/createFixture');
-    const { generate: items } = require('../helpers/getConfig')();
+// TODO Need to add options support from arguments
+module.exports = function generate(url, options = {}) {
+    let status;
 
-    items.forEach(({ id, url, description, filename, options = {} }) => {
-        let status;
-
-        fetch(url, options)
-            .then(d => {
-                status = d.status;
-                return d.json();
-            })
-            .then(data => {
-                const { method } = options;
-
-                createFixture({
-                    id,
-                    data,
-                    description,
-                    filename,
-                    method,
-                    status,
-                    url,
-                });
-            })
-            .catch(err => console.err(err));
-    });
-}
-
+    fetch(url, options)
+        .then(d => {
+            status = d.status;
+            return d.json();
+        })
+        .then(data => {
+            createFixture({
+                data,
+                status,
+                url,
+            });
+        })
+        .catch(err => console.error(err));
+};
