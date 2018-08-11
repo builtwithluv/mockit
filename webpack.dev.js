266814c44b0f805a4e83c66096fa770caa6952b6
Cheng Ly <chengsieuly@gmail.com>
2018-08-11 16:39:15 -0700

    Refactor webpack config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,43 +1,14 @@
-const path = require('path');
 const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const common = require('./webpack.common');
+
 module.exports = {
+    ...common,
     entry: [
         'react-hot-loader/patch',
         './src/client/index.jsx',
     ],
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'fonts/'
-                    }
-                }]
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx'],
-        alias: {
-            '@': path.resolve('src'),
-            '@client': path.resolve('src', 'client'),
-            '@server': path.resolve('src', 'server'),
-        },
-    },
     output: {
         path: __dirname + '/dist',
         publicPath: '/',
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,41 +1,12 @@
-const path = require('path');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const common = require('./webpack.common');
+
 module.exports = {
+    ...common,
     entry: {
         'client/bundle': './src/client/index.jsx',
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: ['babel-loader'],
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-                use: [{
-                    loader: 'file-loader',
-                    options: {
-                        name: '[name].[ext]',
-                        outputPath: 'client/fonts'
-                    }
-                }]
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['*', '.js', '.jsx'],
-        alias: {
-            '@': path.resolve('src'),
-            '@client': path.resolve('src', 'client'),
-            '@server': path.resolve('src', 'server'),
-        },
     },
     output: {
         path: __dirname + '/lib',
